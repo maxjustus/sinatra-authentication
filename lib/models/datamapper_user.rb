f9c28ad80fd58abacfb0bf7e5bf6cb5619f21214
Max <max@gregory.(none)>
2009-10-01 14:43:27 -0500

    made compatible with datamapper 0.10.0

--- lib/models/datamapper_user.rb
+++ lib/models/datamapper_user.rb
@@ -1,14 +1,17 @@
 class DmUser
   include DataMapper::Resource
 
-  attr_accessor :password, :password_confirmation
-
-  property :id, Serial, :protected => true
+  property :id, Serial
   property :email, String, :key => true, :nullable => false, :length => (5..40), :unique => true, :format => :email_address
   property :hashed_password, String
-  property :salt, String, :protected => true, :nullable => false
+  property :salt, String, :nullable => false
   property :created_at, DateTime
   property :permission_level, Integer, :default => 1
+
+  attr_accessor :password, :password_confirmation
+  #protected equievelant? :protected => true doesn't exist in dm 0.10.0
+  #protected :id, :salt
+  #doesn't behave correctly, I'm not even sure why I did this.
 
   validates_present :password_confirmation, :unless => Proc.new { |t| t.hashed_password }
   validates_present :password, :unless => Proc.new { |t| t.hashed_password }
