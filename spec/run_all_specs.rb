756953d72e2c70a81eeca3b19a2e06c0edeb0255
Joshua Mervine <joshua@mervine.net>
2012-05-30 10:53:31 -0700

    reverting commented out test calls

--- spec/run_all_specs.rb
+++ spec/run_all_specs.rb
@@ -3,7 +3,6 @@
 specs = ['sequel_model', 'dm_model', 'mm_model', 'tc_model', 'mongoid_model']
 specs.each do |spec|
   spec_path = File.dirname(__FILE__) + "/unit/#{spec}_spec.rb"
-  puts spec_path
-  #@output = %x[rspec #{spec_path}]
-  #puts @output
+  @output = %x[rspec #{spec_path}]
+  puts @output
 end
